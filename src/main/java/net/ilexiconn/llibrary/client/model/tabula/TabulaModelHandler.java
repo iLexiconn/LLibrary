207a31f574a94dc8d639f6b514078ac01bd8898a
iLexiconn <ilexiconn@gmail.com>
2016-03-24 21:56:53 +0100

    Fix tyos

--- src/main/java/net/ilexiconn/llibrary/client/model/tabula/TabulaModelHandler.java
+++ src/main/java/net/ilexiconn/llibrary/client/model/tabula/TabulaModelHandler.java
@@ -41,9 +41,8 @@ public enum TabulaModelHandler {
      *
      * @param stream the model.json input stream
      * @return the new {@link TabulaModelContainer} instance
-     * @throws IOException if the file can't be found
      */
-    public TabulaModelContainer loadTabulaModel(InputStream stream) throws IOException {
+    public TabulaModelContainer loadTabulaModel(InputStream stream) {
         return gson.fromJson(new InputStreamReader(stream), TabulaModelContainer.class);
     }
 
