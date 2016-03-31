6af88ba5a7680fdb89c007d6fc2ec7cc34d25d80
gegy1000 <gegy1000@gmail.com>
2016-03-31 15:58:04 +0200

    Add rendering hooks

---
+++ src/main/java/net/ilexiconn/llibrary/client/event/PlayerModelEvent.java
@@ -0,0 +1,49 @@
+package net.ilexiconn.llibrary.client.event;
+
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class PlayerModelEvent extends Event {
+    public final ModelPlayer model;
+
+    public PlayerModelEvent(ModelPlayer model) {
+        this.model = model;
+    }
+
+    public static class Construct extends PlayerModelEvent {
+        public Construct(ModelPlayer model) {
+            super(model);
+        }
+    }
+
+    public static class SetRotationAngles extends Render {
+        public SetRotationAngles(ModelPlayer model, EntityPlayer player, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+            super(model, player, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale);
+        }
+    }
+
+    public static class Render extends PlayerModelEvent {
+        public final EntityPlayer entityPlayer;
+        public final float limbSwing;
+        public final float limbSwingAmount;
+        public final float rotation;
+        public final float rotationYaw;
+        public final float rotationPitch;
+        public final float scale;
+
+        public Render(ModelPlayer model, EntityPlayer player, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+            super(model);
+            this.entityPlayer = player;
+            this.limbSwing = limbSwing;
+            this.limbSwingAmount = limbSwingAmount;
+            this.rotation = rotation;
+            this.rotationYaw = rotationYaw;
+            this.rotationPitch = rotationPitch;
+            this.scale = scale;
+        }
+    }
+}
---
+++ src/main/java/net/ilexiconn/llibrary/server/asm/LLibraryASMHandler.java
@@ -0,0 +1,40 @@
+package net.ilexiconn.llibrary.server.asm;
+
+import net.ilexiconn.llibrary.client.event.PlayerModelEvent;
+import net.ilexiconn.llibrary.client.event.RenderArmEvent;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+
+public class LLibraryASMHandler {
+    public static boolean renderLeftArmPre(AbstractClientPlayer player, RenderPlayer renderPlayer) {
+        return MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Left.Pre(player, renderPlayer));
+    }
+
+    public static void renderLeftArmPost(AbstractClientPlayer player, RenderPlayer renderPlayer) {
+        MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Left.Post(player, renderPlayer));
+    }
+
+    public static boolean renderRightArmPre(AbstractClientPlayer player, RenderPlayer renderPlayer) {
+        return MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Right.Pre(player, renderPlayer));
+    }
+
+    public static void renderRightArmPost(AbstractClientPlayer player, RenderPlayer renderPlayer) {
+        MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Right.Post(player, renderPlayer));
+    }
+
+    public static void setRotationAngles(ModelPlayer model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.SetRotationAngles(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
+    }
+
+    public static void constructModel(ModelPlayer model) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Construct(model));
+    }
+
+    public static void renderModel(ModelPlayer model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Render(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
+    }
+}
