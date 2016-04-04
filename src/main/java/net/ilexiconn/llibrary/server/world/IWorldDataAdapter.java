4c3660c3aa70e108060c9fd011f0e6c199fdb018
iLexiconn <ilexiconn@gmail.com>
2016-04-04 19:44:40 +0200

    Added world save handler

---
+++ src/main/java/net/ilexiconn/llibrary/server/world/IWorldDataAdapter.java
@@ -0,0 +1,33 @@
+package net.ilexiconn.llibrary.server.world;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+/**
+ * @author iLexiconn
+ * @since 1.1.1
+ */
+public interface IWorldDataAdapter {
+    /**
+     * This ID will be used for as file name.
+     *
+     * @return the adapter id
+     */
+    String getID();
+
+    /**
+     * Load data from the world.
+     *
+     * @param compound the compound tag
+     * @param world    the world instance
+     */
+    void loadNBTData(NBTTagCompound compound, World world);
+
+    /**
+     * Save data to the world.
+     *
+     * @param compound the compound tag
+     * @param world    the world instance
+     */
+    void saveNBTData(NBTTagCompound compound, World world);
+}
