0d9cfe0a95e7ac9f57aed6ac78b1c2a9cde087f8
iLexiconn <ilexiconn@gmail.com>
2016-04-06 17:44:34 +0200

    Release 1.2.0

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "llibrary", name = "LLibrary", version = LLibrary.VERSION)
 public class LLibrary {
-    public static final String VERSION = "1.2.0-develop";
+    public static final String VERSION = "1.2.0";
 
     @SidedProxy(serverSide = "net.ilexiconn.llibrary.server.ServerProxy", clientSide = "net.ilexiconn.llibrary.client.ClientProxy")
     public static ServerProxy PROXY;
