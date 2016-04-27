82cd9cb7455e75de1640a2eb15e8aeba41aa0e66
gegy1000 <gegy1000@gmail.com>
2016-04-27 17:03:50 +0200

    Fix potential memory leaks

--- src/main/java/net/ilexiconn/llibrary/client/model/tabula/TabulaModel.java
+++ src/main/java/net/ilexiconn/llibrary/client/model/tabula/TabulaModel.java
@@ -23,14 +23,13 @@ import java.util.Map;
 public class TabulaModel extends AdvancedModelBase {
     protected Map<String, AdvancedModelRenderer> cubes = new HashMap<>();
     protected List<AdvancedModelRenderer> rootBoxes = new ArrayList<>();
-    protected TabulaModelContainer container;
     protected ITabulaModelAnimator tabulaAnimator;
     protected Map<String, AdvancedModelRenderer> identifierMap = new HashMap<>();
+    protected double[] scale;
 
     public TabulaModel(TabulaModelContainer container, ITabulaModelAnimator tabulaAnimator) {
         this.textureWidth = container.getTextureWidth();
         this.textureHeight = container.getTextureHeight();
-        this.container = container;
         this.tabulaAnimator = tabulaAnimator;
         for (TabulaCubeContainer cube : container.getCubes()) {
             this.parseCube(cube, null);
@@ -85,8 +84,7 @@ public class TabulaModel extends AdvancedModelBase {
     public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float rotationYaw, float rotationPitch, float scale) {
         this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, rotationYaw, rotationPitch, scale, entity);
         GlStateManager.pushMatrix();
-        double[] modelScale = this.container.getScale();
-        GlStateManager.scale(modelScale[0], modelScale[1], modelScale[2]);
+        GlStateManager.scale(this.scale[0], this.scale[1], this.scale[2]);
         for (AdvancedModelRenderer box : this.rootBoxes) {
             box.render(scale);
         }
