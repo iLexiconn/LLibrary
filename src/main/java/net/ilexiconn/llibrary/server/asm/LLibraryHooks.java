181466ffbba5068927f0fecc1cd0445d6154889c
gegy1000 <gegy1000@gmail.com>
2016-06-05 16:26:00 +0200

    LLibraryHooks class

---
+++ src/main/java/net/ilexiconn/llibrary/server/asm/LLibraryHooks.java
@@ -0,0 +1,49 @@
+package net.ilexiconn.llibrary.server.asm;
+
+import net.ilexiconn.llibrary.client.event.PlayerModelEvent;
+import net.ilexiconn.llibrary.client.event.RenderArmEvent;
+import net.ilexiconn.llibrary.server.util.EnumHandSide;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import org.lwjgl.opengl.GL11;
+
+public class LLibraryHooks {
+    @SuppressWarnings("unused")
+    public static void renderArm(RenderPlayer renderPlayer, EntityPlayer player, EnumHandSide side) {
+        GL11.glColor3f(1.0F, 1.0F, 1.0F);
+        renderPlayer.modelBipedMain.onGround = 0.0F;
+        renderPlayer.modelBipedMain.setRotationAngles(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F, player);
+        if (!MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Pre((AbstractClientPlayer) player, renderPlayer, renderPlayer.modelBipedMain, side))) {
+            GL11.glEnable(GL11.GL_BLEND);
+            renderPlayer.modelBipedMain.bipedRightArm.render(0.0625F);
+            GL11.glDisable(GL11.GL_BLEND);
+            MinecraftForge.EVENT_BUS.post(new RenderArmEvent.Post((AbstractClientPlayer) player, renderPlayer, renderPlayer.modelBipedMain, side));
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static ModelBiped assignModel(RenderPlayer renderPlayer, ModelBiped model) {
+        PlayerModelEvent.Assign event = new PlayerModelEvent.Assign(renderPlayer, model);
+        MinecraftForge.EVENT_BUS.post(event);
+        return event.getModel();
+    }
+
+    @SuppressWarnings("unused")
+    public static void setRotationAngles(ModelBiped model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.SetRotationAngles(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
+    }
+
+    @SuppressWarnings("unused")
+    public static void renderModel(ModelBiped model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Render(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
+    }
+
+    @SuppressWarnings("unused")
+    public static void constructModel(ModelBiped model) {
+        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Construct(model));
+    }
+}
--- src/main/java/net/ilexiconn/llibrary/server/asm/transformer/ModelBipedTransformer.java
+++ src/main/java/net/ilexiconn/llibrary/server/asm/transformer/ModelBipedTransformer.java
@@ -1,10 +1,5 @@
 package net.ilexiconn.llibrary.server.asm.transformer;
 
-import net.ilexiconn.llibrary.client.event.PlayerModelEvent;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.MinecraftForge;
 import org.objectweb.asm.tree.*;
 
 public class ModelBipedTransformer implements ITransformer {
@@ -28,7 +23,7 @@ public class ModelBipedTransformer implements ITransformer {
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 4));
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 5));
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 6));
-                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/transformer/ModelBipedTransformer", "setRotationAngles", "(Lnet/minecraft/client/model/ModelBiped;Lnet/minecraft/entity/Entity;FFFFFF)V", false));
+                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/LLibraryHooks", "setRotationAngles", "(Lnet/minecraft/client/model/ModelBiped;Lnet/minecraft/entity/Entity;FFFFFF)V", false));
                     }
                 }
             } else if (methodNode.name.equals("render") || methodNode.name.equals("func_78088_a")) {
@@ -43,7 +38,7 @@ public class ModelBipedTransformer implements ITransformer {
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 5));
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 6));
                         insnList.insertBefore(insnNode, new VarInsnNode(FLOAD, 7));
-                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/transformer/ModelBipedTransformer", "renderModel", "(Lnet/minecraft/client/model/ModelBiped;Lnet/minecraft/entity/Entity;FFFFFF)V", false));
+                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/LLibraryHooks", "renderModel", "(Lnet/minecraft/client/model/ModelBiped;Lnet/minecraft/entity/Entity;FFFFFF)V", false));
                     }
                 }
             } else if (methodNode.name.equals("<init>") && methodNode.desc.equals("(FFII)V")) {
@@ -51,25 +46,10 @@ public class ModelBipedTransformer implements ITransformer {
                 for (AbstractInsnNode insnNode : insnList.toArray()) {
                     if (insnNode.getOpcode() == RETURN) {
                         insnList.insertBefore(insnNode, new VarInsnNode(ALOAD, 0));
-                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/transformer/ModelBipedTransformer", "constructModel", "(Lnet/minecraft/client/model/ModelBiped;)V", false));
+                        insnList.insertBefore(insnNode, new MethodInsnNode(INVOKESTATIC, "net/ilexiconn/llibrary/server/asm/LLibraryHooks", "constructModel", "(Lnet/minecraft/client/model/ModelBiped;)V", false));
                     }
                 }
             }
         }
-    }
-
-    @SuppressWarnings("unused")
-    public static void setRotationAngles(ModelBiped model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
-        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.SetRotationAngles(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
-    }
-
-    @SuppressWarnings("unused")
-    public static void renderModel(ModelBiped model, Entity entity, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float scale) {
-        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Render(model, (EntityPlayer) entity, limbSwing, limbSwingAmount, rotation, rotationYaw, rotationPitch, scale));
-    }
-
-    @SuppressWarnings("unused")
-    public static void constructModel(ModelBiped model) {
-        MinecraftForge.EVENT_BUS.post(new PlayerModelEvent.Construct(model));
     }
 }
