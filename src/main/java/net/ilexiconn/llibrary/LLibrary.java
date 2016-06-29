918217ca871ab6f5f5b5fbab3bdc3c005d9786f5
iLexiconn <me@ilexiconn.net>
2016-06-29 10:26:09 +0200

    Fix annotations

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -2,24 +2,18 @@ package net.ilexiconn.llibrary;
 
 import net.ilexiconn.llibrary.server.ServerProxy;
 import net.ilexiconn.llibrary.server.capability.IEntityDataCapability;
-import net.ilexiconn.llibrary.server.config.ConfigHandler;
 import net.ilexiconn.llibrary.server.config.LLibraryConfig;
 import net.ilexiconn.llibrary.server.network.*;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.File;
 
 @Mod(modid = "llibrary", name = "LLibrary", version = LLibrary.VERSION, guiFactory = "net.ilexiconn.llibrary.client.gui.LLibraryGUIFactory")
 public class LLibrary {
@@ -50,13 +44,5 @@ public class LLibrary {
     @Mod.EventHandler
     public void onPostInit(FMLPostInitializationEvent event) {
         LLibrary.PROXY.onPostInit();
-    }
-
-    @Mod.EventHandler
-    public void onModConstruct(FMLConstructionEvent event) {
-        for (ModContainer mod : Loader.instance().getModList()) {
-            ConfigHandler.INSTANCE.injectConfig(mod, event.getASMHarvestedData(), new File("."));
-            NetworkHandler.INSTANCE.injectNetworkWrapper(mod, event.getASMHarvestedData());
-        }
     }
 }
