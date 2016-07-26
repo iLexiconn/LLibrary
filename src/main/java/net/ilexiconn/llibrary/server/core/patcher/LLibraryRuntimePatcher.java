93ecea2aba331b730083d05651224a5cc15cd3f3
iLexiconn <me@ilexiconn.net>
2016-07-26 18:09:20 +0200

    ASM helper cleanup

--- src/main/java/net/ilexiconn/llibrary/server/core/patcher/LLibraryRuntimePatcher.java
+++ src/main/java/net/ilexiconn/llibrary/server/core/patcher/LLibraryRuntimePatcher.java
@@ -22,7 +22,7 @@ public class LLibraryRuntimePatcher extends RuntimePatcher {
     public void onInit() {
         patchClass(Locale.class)
             .patchMethod("loadLocaleDataFiles", IResourceManager.class, List.class, void.class)
-                .insertBefore(_method("format", String.class, Object[].class, String.class), method -> {
+                .apply(Patch.BEFORE, at(At.METHOD, "format", String.class, Object[].class, String.class), method -> {
                     method.field(GETSTATIC, LanguageHandler.class, "INSTANCE", LanguageHandler.class);
                     method.var(ALOAD, 4).var(ALOAD, 0);
                     method.field(GETFIELD, Locale.class, "field_135032_a", Map.class);
@@ -31,31 +31,31 @@ public class LLibraryRuntimePatcher extends RuntimePatcher {
 
         patchClass(ModelBiped.class)
             .patchMethod("setRotationAngles", 6, float.class, Entity.class, void.class)
-                .insertBefore(_return(), method -> {
+                .apply(Patch.BEFORE, at(At.RETURN), method -> {
                     method.var(ALOAD, 0).var(ALOAD, 7).var(FLOAD, 1, 6);
                     method.method(INVOKESTATIC, LLibraryHooks.class, "setRotationAngles", ModelBiped.class, Entity.class, 6, float.class, void.class);
                 }).pop()
             .patchMethod("render", Entity.class, 6, float.class, void.class)
-                .insertBefore(_return(), method -> {
+                .apply(Patch.BEFORE, at(At.RETURN), method -> {
                     method.var(ALOAD, 0, 1).var(FLOAD, 2, 7);
                     method.method(INVOKESTATIC, LLibraryHooks.class, "renderModel", ModelBiped.class, Entity.class, 6, float.class, void.class);
                 }).pop()
             .patchMethod("<init>", float.class, float.class, int.class, int.class, void.class)
-                .insertBefore(_return(), method -> {
+                .apply(Patch.BEFORE, at(At.RETURN), method -> {
                     method.var(ALOAD, 0);
                     method.method(INVOKESTATIC, LLibraryHooks.class, "constructModel", ModelBiped.class, void.class);
                 }).pop();
 
         patchClass(RenderPlayer.class)
             .patchMethod("renderFirstPersonArm", EntityPlayer.class, void.class)
-                .set(method -> {
+                .apply(Patch.REPLACE, method -> {
                     method.var(ALOAD, 0, 1);
                     method.field(GETSTATIC, EnumHandSide.class, "RIGHT", EnumHandSide.class);
                     method.method(INVOKESTATIC, LLibraryHooks.class, "renderArm", RenderPlayer.class, EntityPlayer.class, EnumHandSide.class, void.class);
                     method.node(RETURN);
                 }).pop()
             .patchMethod("<init>", void.class)
-                .insertBefore(_node(PUTFIELD), method -> {
+                .apply(Patch.BEFORE, at(At.NODE, PUTFIELD), method -> {
                     if (((FieldInsnNode) method.getInsnNode()).name.equals(MappingHandler.INSTANCE.getFieldMapping(RenderPlayer.class, "modelBipedMain"))) {
                         method.var(ALOAD, 0);
                         method.method(INVOKESTATIC, LLibraryHooks.class, "assignModel", ModelBiped.class, RenderPlayer.class, ModelBiped.class);
@@ -64,7 +64,7 @@ public class LLibraryRuntimePatcher extends RuntimePatcher {
 
         patchClass(MinecraftServer.class)
             .patchMethod("run", void.class)
-                .replace(_ldc(50L), method -> {
+                .apply(Patch.REPLACE_NODE, at(At.LDC, 50L), method -> {
                     method.field(GETSTATIC, TickRateHandler.class, "INSTANCE", TickRateHandler.class);
                     method.method(INVOKEVIRTUAL, TickRateHandler.class, "getTickRate", long.class);
                 }).pop();
