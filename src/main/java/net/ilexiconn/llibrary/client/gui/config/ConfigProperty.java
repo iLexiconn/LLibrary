db58754efb04ddb434a000a0527ecd23d2ef6313
gegy1000 <gegy1000@gmail.com>
2016-08-27 10:58:45 +0200

    Allow custom elements for ConfigProperty

--- src/main/java/net/ilexiconn/llibrary/client/gui/config/ConfigProperty.java
+++ src/main/java/net/ilexiconn/llibrary/client/gui/config/ConfigProperty.java
@@ -1,15 +1,24 @@
 package net.ilexiconn.llibrary.client.gui.config;
 
+import net.ilexiconn.llibrary.client.gui.element.Element;
 import net.ilexiconn.llibrary.server.util.IValueAccess;
 import net.minecraftforge.common.config.Property;
+
+import java.util.function.Function;
 
 public class ConfigProperty<T> {
     private IValueAccess<T> value;
     private Property.Type type;
+    private Function<PropertyData, Element<ConfigGUI>> elementProvider;
 
     public ConfigProperty(IValueAccess<T> value, Property.Type type) {
+        this(value, type, null);
+    }
+
+    public ConfigProperty(IValueAccess<T> value, Property.Type type, Function<PropertyData, Element<ConfigGUI>> elementProvider) {
         this.value = value;
         this.type = type;
+        this.elementProvider = elementProvider;
     }
 
     public T get() {
@@ -22,5 +31,39 @@ public class ConfigProperty<T> {
 
     public Property.Type getType() {
         return this.type;
+    }
+
+    public Function<PropertyData, Element<ConfigGUI>> getElementProvider() {
+        return this.elementProvider;
+    }
+
+    public static class PropertyData {
+        private final ConfigGUI gui;
+        private final ConfigProperty property;
+        private final int x;
+        private final int y;
+
+        public PropertyData(ConfigGUI gui, ConfigProperty property, int x, int y) {
+            this.gui = gui;
+            this.property = property;
+            this.x = x;
+            this.y = y;
+        }
+
+        public ConfigProperty getProperty() {
+            return this.property;
+        }
+
+        public int getX() {
+            return  this.x;
+        }
+
+        public int getY() {
+            return  this.y;
+        }
+
+        public ConfigGUI getGUI() {
+            return this.gui;
+        }
     }
 }
