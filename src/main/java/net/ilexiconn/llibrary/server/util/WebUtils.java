b72514f61ec093042c4005289ac0bb0850be6c1d
gegy1000 <gegy1000@gmail.com>
2017-02-21 07:51:57 +0200

    Cleanup WebUtils

--- src/main/java/net/ilexiconn/llibrary/server/util/WebUtils.java
+++ src/main/java/net/ilexiconn/llibrary/server/util/WebUtils.java
@@ -1,9 +1,11 @@
 package net.ilexiconn.llibrary.server.util;
 
-import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import net.ilexiconn.llibrary.LLibrary;
 import net.minecraft.crash.CrashReport;
-import net.minecraft.util.Tuple;
+import org.apache.commons.io.IOUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -13,9 +15,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.function.Consumer;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Utilities to access data from the internet. All methods throw out an error to the console if something goes wrong.
@@ -26,29 +28,7 @@ import java.util.function.Consumer;
 public class WebUtils {
     public static final String PASTEBIN_URL_PREFIX = "http://pastebin.com/raw.php?i=";
 
-    private static final Queue<Tuple<String, Consumer<String>>> DOWNLOAD_QUEUE = new LinkedBlockingDeque<>();
-
-    static {
-        Thread downloadThread = new Thread(() -> {
-            while (true) {
-                try {
-                    if (!DOWNLOAD_QUEUE.isEmpty()) {
-                        Tuple<String, Consumer<String>> request = DOWNLOAD_QUEUE.poll();
-                        String url = request.getFirst();
-                        Consumer<String> callback = request.getSecond();
-                        String downloaded = WebUtils.readURL(url);
-                        callback.accept(downloaded);
-                    }
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        downloadThread.setName("LLibrary Download Thread");
-        downloadThread.setDaemon(true);
-        downloadThread.start();
-    }
+    private static final ListeningExecutorService EXECUTOR = MoreExecutors.listeningDecorator(new ThreadPoolExecutor(1, 1, 5, TimeUnit.SECONDS, new LinkedBlockingQueue<>()));
 
     /**
      * Get content from a pastebin file as String, split to multiple lines using newlines. Returns null if something goes wrong.
@@ -77,17 +57,8 @@ public class WebUtils {
      * @return the content from the url or null
      */
     public static String readURL(String url) {
-        try {
-            String text = "";
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
-            String currentLine;
-
-            while ((currentLine = reader.readLine()) != null) {
-                text += currentLine + "\r\n";
-            }
-            reader.close();
-
-            return text;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+            return IOUtils.toString(reader);
         } catch (IOException e) {
             LLibrary.LOGGER.error(CrashReport.makeCrashReport(e, "Failed to receive data from URL: " + url).getCompleteReport());
             return null;
@@ -101,17 +72,8 @@ public class WebUtils {
      * @return the content from a url or null
      */
     public static List<String> readURLAsList(String url) {
-        try {
-            List<String> text = Lists.newArrayList();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
-            String currentLine;
-
-            while ((currentLine = reader.readLine()) != null) {
-                text.add(currentLine);
-            }
-            reader.close();
-
-            return text;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+            return IOUtils.readLines(reader);
         } catch (IOException e) {
             LLibrary.LOGGER.error(CrashReport.makeCrashReport(e, "Failed to receive data from URL: " + url).getCompleteReport());
             return null;
@@ -125,8 +87,8 @@ public class WebUtils {
      * @return the image object or null
      */
     public static BufferedImage downloadImage(String url) {
-        try {
-            return ImageIO.read(new BufferedInputStream(new URL(url).openStream()));
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream())){
+            return ImageIO.read(in);
         } catch (IOException e) {
             LLibrary.LOGGER.error(CrashReport.makeCrashReport(e, "Failed to receive data from URL: " + url).getCompleteReport());
             return null;
@@ -137,42 +99,39 @@ public class WebUtils {
      * Get content from a url as String, split to multiple lines using newlines, asynchronously. Returns null if something goes wrong.
      *
      * @param url the url to read data from
-     * @param callback callback for when the data is retrieved
+     * @return the future for the returned data
      */
-    public static void readURLAsync(String url, Consumer<String> callback) {
-        DOWNLOAD_QUEUE.add(new Tuple<>(url, callback));
+    public static ListenableFuture<String> readURLAsync(String url) {
+        return EXECUTOR.submit(() -> WebUtils.readURL(url));
     }
 
     /**
      * Get content from a url as List, with every newline as a new entry, asynchronously. Returns null if something goes wrong.
      *
      * @param url the url to read data from
-     * @param callback callback for when the data is retrieved
+     * @return the future for the returned data
      */
-    public static void readURLAsListAsync(String url, Consumer<List<String>> callback) {
-        DOWNLOAD_QUEUE.add(new Tuple<>(url, (text) -> {
-            String[] lines = text.split("\r\n");
-            callback.accept(Lists.newArrayList(lines));
-        }));
+    public static ListenableFuture<List<String>> readURLAsListAsync(String url) {
+        return EXECUTOR.submit(() -> WebUtils.readURLAsList(url));
     }
 
     /**
      * Get content from a pastebin file as String, split to multiple lines using newlines asynchronously. Returns null if something goes wrong.
      *
      * @param pasteID the paste id to read data from
-     * @param callback callback for when the data is received
+     * @return the future for the returned data
      */
-    public static void readPastebinAsync(String pasteID, Consumer<String> callback) {
-        readURLAsync(WebUtils.PASTEBIN_URL_PREFIX + pasteID, callback);
+    public static ListenableFuture<String> readPastebinAsync(String pasteID) {
+        return EXECUTOR.submit(() -> WebUtils.readPastebin(pasteID));
     }
 
     /**
      * Get content from a pastebin file as String, with every newline as a new entry, asynchronously. Returns null if something goes wrong.
      *
      * @param pasteID the paste id to read data from
-     * @param callback callback for when the data is received
+     * @return the future for the returned data
      */
-    public static void readPastebinAsListAsync(String pasteID, Consumer<String> callback) {
-        readURLAsync(WebUtils.PASTEBIN_URL_PREFIX + pasteID, callback);
+    public static ListenableFuture<List<String>> readPastebinAsListAsync(String pasteID) {
+        return EXECUTOR.submit(() -> WebUtils.readURLAsList(pasteID));
     }
 }
