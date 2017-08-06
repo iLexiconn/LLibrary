a5f84b8afd7e62d9e1b4abb3e61067ee2cb2d885
gegy1000 <gegy1000@gmail.com>
2017-08-06 19:02:58 +0200

    Remove ApplyRenderRotationsEvent generic

--- src/main/java/net/ilexiconn/llibrary/server/core/patcher/LLibraryHooks.java
+++ src/main/java/net/ilexiconn/llibrary/server/core/patcher/LLibraryHooks.java
@@ -94,13 +94,13 @@ public class LLibraryHooks {
 
     @SideOnly(Side.CLIENT)
     @SuppressWarnings("unused")
-    public static <T extends EntityLivingBase> void applyRotationsPre(RenderLivingBase<T> renderer, T entity, float partialTicks) {
-        MinecraftForge.EVENT_BUS.post(new ApplyRenderRotationsEvent.Pre<>(renderer, entity, partialTicks));
+    public static void applyRotationsPre(RenderLivingBase<EntityLivingBase> renderer, EntityLivingBase entity, float partialTicks) {
+        MinecraftForge.EVENT_BUS.post(new ApplyRenderRotationsEvent.Pre(renderer, entity, partialTicks));
     }
 
     @SideOnly(Side.CLIENT)
     @SuppressWarnings("unused")
-    public static <T extends EntityLivingBase> void applyRotationsPost(RenderLivingBase<T> renderer, T entity, float partialTicks) {
-        MinecraftForge.EVENT_BUS.post(new ApplyRenderRotationsEvent.Post<>(renderer, entity, partialTicks));
+    public static void applyRotationsPost(RenderLivingBase<EntityLivingBase> renderer, EntityLivingBase entity, float partialTicks) {
+        MinecraftForge.EVENT_BUS.post(new ApplyRenderRotationsEvent.Post(renderer, entity, partialTicks));
     }
 }
