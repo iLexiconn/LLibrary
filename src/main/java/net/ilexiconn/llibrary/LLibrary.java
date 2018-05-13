46370e199a3c6f6895641dcd24b52364e0e58ffa
gegy1000 <gegy1000@gmail.com>
2018-05-13 09:02:51 +0200

    Version 1.7.10

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -40,7 +40,7 @@ import java.util.Map;
 
 @Mod(modid = "llibrary", name = "LLibrary", version = LLibrary.VERSION, acceptedMinecraftVersions = "1.12.2", certificateFingerprint = "${fingerprint}", guiFactory = "net.ilexiconn.llibrary.client.gui.LLibraryGUIFactory", updateJSON = "https://gist.github.com/gegy1000/a6639456aeb8edd92cbf7cbfcf9d65d9")
 public class LLibrary {
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.7.10";
 
     public static final Logger LOGGER = LogManager.getLogger("LLibrary");
     @SidedProxy(serverSide = "net.ilexiconn.llibrary.server.ServerProxy", clientSide = "net.ilexiconn.llibrary.client.ClientProxy")
