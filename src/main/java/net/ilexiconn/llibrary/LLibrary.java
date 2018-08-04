c55d0c4f8109fa6a5fa827a219d15c6eb5210057
gegy1000 <gegy1000@gmail.com>
2018-08-04 09:30:17 +0200

    Bump version

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -49,7 +49,7 @@ import java.util.Map;
         dependencies = "required-after:forge@[14.23.3.2655,)"
 )
 public class LLibrary {
-    public static final String VERSION = "1.7.14";
+    public static final String VERSION = "1.7.15";
 
     public static final Logger LOGGER = LogManager.getLogger("LLibrary");
     @SidedProxy(serverSide = "net.ilexiconn.llibrary.server.ServerProxy", clientSide = "net.ilexiconn.llibrary.client.ClientProxy")
