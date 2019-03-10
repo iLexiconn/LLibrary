690b79c17d139c4fb8048a95edacf2e2c08f2394
gegy1000 <gegy1000@gmail.com>
2019-03-10 09:38:05 +0200

    Update LLibraryCore: fixes #130, #127, #126, #119, #118, #112, #117

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -49,7 +49,7 @@ import java.util.Map;
         dependencies = "required-after:forge@[14.23.5.2772,)"
 )
 public class LLibrary {
-    public static final String VERSION = "1.7.17";
+    public static final String VERSION = "1.7.18";
 
     public static final Logger LOGGER = LogManager.getLogger("LLibrary");
     @SidedProxy(serverSide = "net.ilexiconn.llibrary.server.ServerProxy", clientSide = "net.ilexiconn.llibrary.client.ClientProxy")
